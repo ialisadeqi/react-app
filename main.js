55e1ecc56750c3b8a62c0fec4bb34f71fa50e8ac
ialisadeqi <77195340+ialisadeqi@users.noreply.github.com>
2023-06-20 21:32:56 +0330

    push to github.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const line = document.getElementById("borderLine");
 const nav = document.getElementById("nav");
 const parent = document.getElementById("parent");
+const btn = document.querySelector(".btn");
 
 nav.addEventListener("mousemove", (e) => {
   line.style.left = e.clientX + "px";
